616d17a1991228ad7c3755e58b1325f421fc0e19
Programmer-Jaf <programmerjaf@gmail.com>
2025-06-22 21:02:40 +0600

    learn about JSX in react

---
+++ 02__JSX-In-React/src/App.jsx
@@ -0,0 +1,29 @@
+// Copyright 2025 Programmer-Jaf
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ConvertedJSX } from "./components/ConvertedJSX"
+import { LearnJSX } from "./components/Learn-JSX"
+
+const App = () =>{
+  return (
+    <div>
+      {/* import Component */}
+      <LearnJSX/>
+      <hr />
+      Converted JSX After Rendered
+      <ConvertedJSX/>
+    </div>
+  )
+}
+export default App
---
+++ 02__JSX-In-React/src/components/ConvertedJSX.jsx
@@ -0,0 +1,25 @@
+// Copyright 2025 Programmer-Jaf
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+export const ConvertedJSX = () => {
+  return (
+    _jsxs("div", {
+      children: [/*#__PURE__*/_jsx("h1", {
+        children: "This is a Heading Tag"
+      }), /*#__PURE__*/_jsx("p", {
+        children: "This is a paragraph"
+      })]
+    })
+  );
+};
