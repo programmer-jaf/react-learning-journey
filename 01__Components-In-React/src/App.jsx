4bb18dbfb3b8423a2b41adc7ec4a5e0547727ac0
Programmer-Jaf <programmerjaf@gmail.com>
2025-06-22 19:23:42 +0600

    Add Greeting component and update App to include it

--- 01__Components-In-React/src/App.jsx
+++ 01__Components-In-React/src/App.jsx
@@ -1,10 +1,14 @@
 import Component from "./components/Component";
+import Greeting from "./solutions/Greeting";
 
 const App = () => {
   return (
     <div>
 {/* call the component */}
-      <Component/>
+      {/* <Component/> */}
+
+      {/* Challenge-01 */}
+      <Greeting/>
     </div>
   );
 };
