bce2f101e6fdc8d67ccc4b1e235fedad7cfcf82e
Programmer-Jaf <programmerjaf@gmail.com>
2025-06-22 20:42:17 +0600

    Challenge-02 Add Header, MainContent, and Footer components to App

--- 01__Components-In-React/src/App.jsx
+++ 01__Components-In-React/src/App.jsx
@@ -1,14 +1,21 @@
 import Component from "./components/Component";
+import { Footer } from "./solutions/Footer";
 import Greeting from "./solutions/Greeting";
+import { Header } from "./solutions/Header";
+import { MainContent } from "./solutions/MainContent";
 
 const App = () => {
   return (
     <div>
-{/* call the component */}
+      {/* call the component */}
       {/* <Component/> */}
 
       {/* Challenge-01 */}
-      <Greeting/>
+      {/* <Greeting/> */}
+      {/* Challenge-02 */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 };
